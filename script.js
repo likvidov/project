5f8883c006bb653a8f9a746711e4058c24e96f03
likvidov <likvidya@gmail.comm>
2023-06-12 16:00:05 +0300

    Изменил валюту и константы

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let title = "Project";
-let screens = "Простые, Сложные, Интерактивные";
+const title = "Project";
+const screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 10;
 let rollback = 50;
 let fullPrice = 1000;
@@ -10,15 +10,9 @@ console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
 
-console.log("Стоимость верстки экранов " + (screenPrice) + " рублей");
-console.log("Стоимость верстки экранов " + (screenPrice / 80).toFixed(2) + " долларов");
-console.log("Стоимость верстки экранов " + (screenPrice / 2.24).toFixed(2) + " гривен");
-console.log("Стоимость верстки экранов " + (screenPrice / 11.57).toFixed(2) + " юани");
+console.log("Стоимость верстки экранов " + (screenPrice.toFixed(2)) + " рублей");
 
-console.log("Стоимость разработки сайта " + (fullPrice) + " рублей");
-console.log("Стоимость разработки сайта " + (fullPrice / 80).toFixed(2) + " долларов");
-console.log("Стоимость разработки сайта " + (fullPrice / 2.24).toFixed(2) + " гривен");
-console.log("Стоимость разработки сайта " + (fullPrice / 11.57).toFixed(2) + " юани");
+console.log("Стоимость разработки сайта " + (fullPrice.toFixed(2)) + " рублей");
 
 console.log(screens.toLowerCase().split(", "));
 
