58ced39155567bcfe85a8cf317c70451a1a6ea9b
likvidov <likvidya@gmail.comm>
2023-06-12 17:42:38 +0300

    Выполненое второе сложное задание

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
-let num = 266219;
-let result = 1;
-for (let i = 0; i < num.toString().length; i++) {
-  result *= num.toString()[i];
+let lang = prompt('Русский или Английский') ? 'ru' : 'en'
+let arr;
+
+if (lang == 'ru') {
+  arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
 }
-console.log(result)
-let result2 = result ** 3
-console.log(result2)
-console.log(result2.toString().slice(0, 2))
+if (lang == 'en') {
+  arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+}
+
+switch (lang) {
+  case 'ru':
+    arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+    break;
+  case 'en':
+    arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+    break;
+}
+
+let dayofweek = {
+  'ru': ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+  'en': ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
+};
+
+console.log(arr)
+console.log(dayofweek[lang])
+
+let namePerson = 'Артем'
+
+namePerson === 'Артем' ? console.log('директор') : console.log('студент')
+namePerson === 'Александр' ? console.log('преподаватель') : console.log('студент')
