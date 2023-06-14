490974dc76443f224bb6345f4fe7fe5bb9d23963
likvidov <likvidya@gmail.comm>
2023-06-14 19:48:13 +0300

    Изменил dayofweek

--- main.js
+++ main.js
@@ -1,28 +1,25 @@
 let lang = prompt('Русский или Английский') ? 'ru' : 'en'
-let arr;
-
-if (lang == 'ru') {
-  arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-}
-if (lang == 'en') {
-  arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
-}
-
-switch (lang) {
-  case 'ru':
-    arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-    break;
-  case 'en':
-    arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
-    break;
-}
-
-let dayofweek = {
+const dayofweek = {
   'ru': ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
   'en': ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
 };
 
-console.log(arr)
+if (lang == 'ru') {
+  dayofweek['ru'];
+}
+if (lang == 'en') {
+  dayofweek['en'];
+}
+
+switch (lang) {
+  case 'ru':
+    dayofweek['ru'];
+    break;
+  case 'en':
+    dayofweek['en'];
+    break;
+}
+
 console.log(dayofweek[lang])
 
 let namePerson = 'Артем'
