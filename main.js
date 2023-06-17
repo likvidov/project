baf255f1705241862e6202fe854595fde9bf6e8e
likvidov <likvidya@gmail.comm>
2023-06-17 20:39:32 +0300

    Выполнено сложное задание урока 7

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-let arr = ['1312', '3123', '2434', '4231', '5435', '4324', '8432'];
+const dayOfWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const date = new Date();
 
-arr.forEach((item) => {
-  if (item.startsWith('2') || item.startsWith('4')) {
+dayOfWeek.forEach((item, i) => {
+  if (i == date.getDay() - 1) {
+    console.log(`%c${item}`, 'font-weight: bold')
+  } else if (item == 'Суббота' || item == 'Воскресенье') {
+    console.log(`%c${item}`, 'font-style: italic;')
+  } else {
     console.log(item)
   }
 })
-
-for (let i = 1; i <= 100; i++) {
-  let count = 0;
-  if (i === 1) continue;
-  for (let t = 2; t <= i; t++) {
-    if (i % t == 0) {
-      count++;
-    }
-  }
-  if (count <= 1) console.log(i + ' = делители этого числа: 1 и ' + i);
-}
