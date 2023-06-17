a5a7fa1522ae8c0807f7b6da4f106eb63f5aa7b7
likvidov <likvidya@gmail.comm>
2023-06-17 11:15:09 +0300

    Выполненое сложное задание

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const get = function (line) {
-  if (typeof line !== 'string') return 'Аргумент не является строкой';
-  let str = line.trim();
-  if (str.length > 30) return str.slice(0, 29) + '...';
+let arr = ['1312', '3123', '2434', '4231', '5435', '4324', '8432'];
 
-  return str;
+arr.forEach((item) => {
+  if (item.startsWith('2') || item.startsWith('4')) {
+    console.log(item)
+  }
+})
+
+for (let i = 1; i <= 100; i++) {
+  let count = 0;
+  if (i === 1) continue;
+  for (let t = 2; t <= i; t++) {
+    if (i % t == 0) {
+      count++;
+    }
+  }
+  if (count <= 1) console.log(i + ' = делители этого числа: 1 и ' + i);
 }
-
-console.log(get(213))
-console.log(get(' Lorem ipsum dolor sit amet. '));
-console.log(get('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, temporibus?'));
