724b669bda6db7f0dadbaa7f2b69b1167174a744
likvidov <likvidya@gmail.comm>
2023-07-02 19:31:19 +0300

    Исправление ошибок

--- js/script.js
+++ js/script.js
@@ -37,10 +37,10 @@ const appData = {
   init: function () {
     this.addTitle();
 
-    startBtn.addEventListener('click', this.start.bind(appData));
-    resetBtn.addEventListener('click', this.reset.bind(appData));
+    startBtn.addEventListener('click', this.start.bind(this));
+    resetBtn.addEventListener('click', this.reset.bind(this));
     buttonPlus.addEventListener('click', this.addScreenBlock);
-    inputRange.addEventListener('change', this.showRollback.bind(appData));
+    inputRange.addEventListener('change', this.showRollback.bind(this));
     cmsBtn.addEventListener('click', () => {
       hiddenCmsVariants.style.display = hiddenCmsVariants.style.display == 'none' ? 'flex' : 'none'
     })
